7849f5900891115e8bd4bc790007a7cde5df21a6
SensitiveLion <andypassidomo@gmail.com>
2015-06-05 12:03:39 -0400

    added form submit

--- gmail.rb
+++ gmail.rb
@@ -6,4 +6,5 @@ gmail = agent.get("https://www.gmail.com/")
 gmail_form =  gmail.form()
 gmail_form.Email = "testemail"
 gmail_form.Passwd = "password"
+gmail = agent.submit(gmail_form)
 pp gmail
--- reddit.rb
+++ reddit.rb
@@ -6,4 +6,5 @@ reddit = agent.get("https://www.reddit.com/")
 reddit_form =  reddit.forms[1]
 reddit_form.user = "thing"
 reddit_form.passwd = "random"
+reddit = agent.submit(reddit_form)
 pp reddit
