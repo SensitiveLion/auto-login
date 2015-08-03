2b7a770e2fa3ebb570ab4e53eac44beec693ca83
SensitiveLion <andypassidomo@gmail.com>
2015-08-03 13:19:31 -0400

    took out pry

--- instagram.rb
+++ instagram.rb
@@ -3,6 +3,7 @@ require "mechanize"
 
 agent = Mechanize.new
 @instagram = agent.get("https://instagram.com")
+@instagram.link_with(text: "Log in").click
 binding.pry
 log_in = @instagram.link_with(:text => "Continue")
 
